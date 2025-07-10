27e0a71fb4f40b0111800787f74ec148b9c04e49
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-10 06:40:54 +0100

    Update 0-databaseconnection.py

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,22 +1,26 @@
 class DatabaseConnection:
+    def __init__(self):
+        self.connection = None
+
     def __enter__(self):
         self.connection = self.connect()
-        return self.connection
+        return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close(self.connection)
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
 
     def connect(self):
         print("[DB] Connection opened.")
         return self
 
-    def close(self, connection):
+    def close(self):
         print("[DB] Connection closed.")
 
     def execute(self, query):
         print(f"[DB] Executing query: {query}")
         return f"Results for query: {query}"
 
-with DatabaseConnection() as db:
-    result = db.execute("SELECT * FROM users")
-    print(result)
+if __name__ == "__main__":
+    with DatabaseConnection() as db:
+        result = db.execute("SELECT * FROM users")
+        print(result)
