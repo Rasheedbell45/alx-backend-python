f183b01fe6825ff044cae3fea8fc6e3ffe4e2210
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-17 23:24:01 +0100

    Update serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,6 +3,8 @@ from .models import CustomUser, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField()
+
     class Meta:
         model = CustomUser
         fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number']
@@ -10,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
 
     class Meta:
         model = Message
@@ -18,8 +21,19 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_messages(self, obj):
+        """Returns serialized messages in the conversation"""
+        messages = obj.messages.all().order_by('-sent_at')
+        return MessageSerializer(messages, many=True).data
+
+    def validate(self, attrs):
+        """Example of using serializers.ValidationError"""
+        if 'participants' in attrs and len(attrs['participants']) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return attrs
