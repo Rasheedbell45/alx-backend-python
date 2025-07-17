e52127f67fc514ed8e1aa9f3e203b71a5391a66e
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-17 23:14:19 +0100

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,1 +1,26 @@
+# chats/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField('CustomUser', related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
+
+  class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='messages_sent')
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message {self.id} from {self.sender.username}"
