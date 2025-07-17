70f3e6ba38f48d50589fd7eb1ba463a36d601a58
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-17 23:09:09 +0100

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,1 +1,16 @@
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
+INSTALLED_APPS = [
+    # ...
+    'rest_framework',
+    # ...
+]
