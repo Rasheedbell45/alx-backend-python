9274589a70fa93129273d43d14038d4b23cc1ced
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 21:05:50 +0100

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -6,3 +6,35 @@ urlpatterns = [
     path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # refresh
+]
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_messages(request):
+    user = request.user
+    messages = Message.objects.filter(user=user)
+    ...
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+class UserMessagesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        messages = Message.objects.filter(user=user)
+        ...
