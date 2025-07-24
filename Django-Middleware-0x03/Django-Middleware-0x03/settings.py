9a09acbc63dfdbc404e2561a4313ad617f185f64
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-24 05:00:54 +0100

    Update settings.py

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -5,9 +5,10 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 
-    # Custom middleware (order matters)
-    'chats.middleware.CombinedMiddleware',                # Logs + Restricts hours
-    'chats.middleware.OffensiveLanguageMiddleware',       # Rate limiter
+    # Custom middleware
+    'chats.middleware.CombinedMiddleware',               # Logs and time-based restriction
+    'chats.middleware.OffensiveLanguageMiddleware',      # Chat message rate-limiting
+    'chats.middleware.RolePermissionMiddleware',         # Role-based access control
 
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
