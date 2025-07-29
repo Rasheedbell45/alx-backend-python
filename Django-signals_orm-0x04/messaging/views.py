a16f5c20894e865aa0c21996aa692fefe910c12b
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-29 21:49:35 +0100

    Create views.py

---
+++ Django-signals_orm-0x04/messaging/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Message
+
+def message_detail(request, message_id):
+    message = get_object_or_404(Message, id=message_id)
+    history = message.history.all().order_by('-edited_at')
+    return render(request, 'messaging/message_detail.html', {'message': message, 'history': history})
