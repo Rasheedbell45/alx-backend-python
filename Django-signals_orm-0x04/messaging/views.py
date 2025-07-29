a7e235b46c086a475c939134719fd49a1178e257
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-29 22:04:48 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.db.models import Prefetch
+from .models import Message
 
 @login_required
 def delete_user(request):
@@ -8,3 +10,18 @@ def delete_user(request):
     user.delete()
     messages.success(request, "Your account has been successfully deleted.")
     return redirect('home')
+
+def get_threaded_conversation(user):
+    top_level_messages = (
+        Message.objects
+        .filter(receiver=user, parent_message__isnull=True)
+        .select_related('sender', 'receiver')  # Efficient foreign key access
+        .prefetch_related(
+            Prefetch(
+                'replies',
+                queryset=Message.objects.select_related('sender').order_by('timestamp')
+            )
+        )
+        .order_by('-timestamp')
+    )
+    return top_level_messages
