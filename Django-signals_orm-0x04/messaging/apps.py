9fb615c13f51a40f231def320359c1de4ccef912
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-29 21:42:26 +0100

    Update apps.py

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -5,3 +5,10 @@ class MessagesConfig(AppConfig):
 
     def ready(self):
         import messages.signals
+
+class MessagingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'messaging'
+
+    def ready(self):
+        import messaging.signals
