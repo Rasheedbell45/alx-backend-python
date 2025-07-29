53755a27a684fc90c04642ea53c44089da1688b0
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-29 21:55:33 +0100

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -3,7 +3,8 @@ from django.dispatch import receiver
 from .models import Message
 from django.db.models.signals import pre_save
 from .models import Message, MessageHistory, Notification
-from notifications.models import Notification
+from django.db.models.signals import post_delete
+from django.contrib.auth.models import User
 
 @receiver(pre_save, sender=Message)
 def log_message_history(sender, instance, **kwargs):
@@ -25,3 +26,10 @@ def log_message_history(sender, instance, **kwargs):
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+@receiver(post_delete, sender=User)
+def cleanup_user_related_data(sender, instance, **kwargs):
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(edited_by=instance).delete()
