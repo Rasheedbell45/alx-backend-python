2cea8c1a945f8840d795470b66171c0ff83d7da6
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-07 00:13:46 +0100

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -34,3 +34,16 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
 }
+
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE', 'messaging_db'),
+        'USER': os.getenv('MYSQL_USER', 'messaging_user'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'your_password'),
+        'HOST': 'db',  # Service name from docker-compose
+        'PORT': '3306',
+    }
+}
